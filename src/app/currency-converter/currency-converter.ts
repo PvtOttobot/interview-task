6b9dd8d5ae59afd64b197b0d8bc8d71ab63c9764
Otto Petersen <danielottojackpetersen2@gmail.com>
2025-07-16 23:45:12 +0100

    feat(currency-converter): add structure

--- src/app/currency-converter/currency-converter.ts
+++ src/app/currency-converter/currency-converter.ts
@@ -10,33 +10,60 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
   providers: [CurrencyBeacon],
   styles: `
     .c-currency-converter {
+    }
+
+    .c-currency-converter {
       display: flex;
       flex-direction: column;
       gap: 10px;
       max-width: 500px;
     }
+
+    .c-currency-converter__from {
+    }
+    .c-currency-converter__to {
+    }
   `,
   template: `
     <form [formGroup]="form" class="c-currency-converter">
-      <label [for]="selectLeftId">Label for left select</label>
-      <select [id]="selectLeftId" formControlName="leftSelect">
-        @for (currency of currenciesResponse()?.response; track currency.id) {
-          <option>{{ currency.short_code }}</option>
-        }
-      </select>
+      <div class="c-currency-converter__from">
+        <h2>From</h2>
+        <select [id]="selectLeftId" formControlName="leftSelect">
+          @for (currency of currenciesResponse()?.response; track currency.id) {
+            <option>{{ currency.short_code }}</option>
+          }
+        </select>
 
-      <label [for]="inputLeftId">Label for left input</label>
-      <input [id]="inputLeftId" formControlName="leftInput" />
+        <label [for]="inputLeftId" [hidden]="true">Amount</label>
+        <input [id]="inputLeftId" [formControl]="form.controls.leftInput" />
+        <label [for]="selectLeftId" [hidden]="true">Currency</label>
+        <p>
+          {{
+            asString(
+              form.controls.leftInput.value,
+              form.controls.leftSelect.value
+            )
+          }}
+        </p>
+      </div>
 
-      <label [for]="selectRightId">Label for right select</label>
-      <select [id]="selectRightId" formControlName="rightSelect">
-        @for (currency of currenciesResponse()?.response; track currency.id) {
-          <option>{{ currency.short_code }}</option>
-        }
-      </select>
+      <div class="c-currency-converter__to">
+        <h2>To</h2>
+        <label [for]="selectRightId" [hidden]="true">Currency</label>
+        <select [id]="selectRightId" formControlName="rightSelect">
+          @for (currency of currenciesResponse()?.response; track currency.id) {
+            <option>{{ currency.short_code }}</option>
+          }
+        </select>
 
-      <label [for]="inputRightId">Label for right input</label>
-      <input [id]="inputRightId" formControlName="rightInput" />
+        <label [for]="inputRightId" [hidden]="true">Amount</label>
+        <input
+          [id]="inputRightId"
+          formControlName="rightInput"
+          [readonly]="true"
+        />
+      </div>
+      <p></p>
     </form>
   `,
 })
@@ -45,10 +72,10 @@ export class CurrencyConverter {
   currenciesResponse = toSignal(this.currencyBeaconService.getCurrencies());
 
   form = new FormGroup({
-    leftSelect: new FormControl(''),
-    leftInput: new FormControl('100'),
-    rightSelect: new FormControl(''),
-    rightInput: new FormControl('100'),
+    leftSelect: new FormControl<string>(''),
+    leftInput: new FormControl<string>('100'),
+    rightSelect: new FormControl<string>(''),
+    rightInput: new FormControl<string>('100'),
   });
 
   selectLeftId = uuidv4();
@@ -57,7 +84,7 @@ export class CurrencyConverter {
   inputRightId = uuidv4();
 
   updateConversion = () => {
-    const convertResponse = this.currencyBeaconService
+    this.currencyBeaconService
       .getConvert({
         from: this.form.controls.leftSelect.value || '',
         to: this.form.controls.rightSelect.value || '',
@@ -65,9 +92,20 @@ export class CurrencyConverter {
       })
       .subscribe((value) => {
         this.form.controls.rightInput.patchValue(
-          value.response.value.toString() || null,
+          value.response.value.toString(),
         );
       });
+  };
+
+  asString = (amount: string | null, shortCode: string | null) => {
+    const currency = this.currenciesResponse()?.response.find((currency) => {
+      return currency.short_code === shortCode;
+    });
+    if (currency) {
+      return `${currency.symbol} ${shortCode}`;
+    } else {
+      return 'No currency selected';
+    }
   };
 
   constructor() {
